87b7890ecd4b116bd974cc111a67b7c0003ce8fd
azzurraquattrini <azzurra.quattrini@studio.unibo.it>
2026-02-05 18:34:39 +0100

    Implemented test methods for BaseCell.reveal() and BaseCell.isRevealed()

--- src/test/java/it/unibo/goldhunt/board/impl/BaseCellTest.java
+++ src/test/java/it/unibo/goldhunt/board/impl/BaseCellTest.java
@@ -1,8 +1,54 @@
 package it.unibo.goldhunt.board.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test; 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.unibo.goldhunt.board.api.Cell;
+import it.unibo.goldhunt.board.api.CellFactory;
 
 public class BaseCellTest {
+    
+    private CellFactory factory;
+    private Cell cell;
+
+    @BeforeEach
+    void init() {
+        this.factory = new BaseCellFactory();
+        this.cell = this.factory.createCell();
+    }
+
+    /**
+     * Tests that a cell is revealed when it is not flagged.
+     */
+    @Test
+    void testRevealWhenNotFlagged() {
+        cell.reveal();
+        assertTrue(cell.isRevealed());
+        assertFalse(cell.isFlagged());
+    }
+    
+    /**
+     * Tests that a revealed cell remains revealed if revealed more than once.
+     */
+    @Test
+    void testRevealWhenAlreadyRevealed() {
+        cell.reveal();
+        cell.reveal();
+        assertTrue(cell.isRevealed());
+    }
+
+    /**
+     * Tests that a flagged cell cannot be revealed.
+     */
+    @Test
+    void testRevealWhenFlagged() {
+        cell.toggleFlag();
+        cell.reveal();
+        assertFalse(cell.isRevealed());
+        assertTrue(cell.isFlagged());
+    }
 
 }
