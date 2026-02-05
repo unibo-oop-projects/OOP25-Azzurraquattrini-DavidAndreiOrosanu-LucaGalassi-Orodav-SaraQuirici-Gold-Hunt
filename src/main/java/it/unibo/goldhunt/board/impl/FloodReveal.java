da55cd55e2f538a28d5514d80a4af85dcae51b3d
azzurraquattrini <azzurra.quattrini@studio.unibo.it>
2026-02-05 14:15:28 +0100

    Implemented FloodReveal.reveal()

--- src/main/java/it/unibo/goldhunt/board/impl/FloodReveal.java
+++ src/main/java/it/unibo/goldhunt/board/impl/FloodReveal.java
@@ -1,17 +1,31 @@
+// AZZU
+
 package it.unibo.goldhunt.board.impl;
 
+import java.util.Objects;
+
 import it.unibo.goldhunt.board.api.Board;
+import it.unibo.goldhunt.board.api.Cell;
 import it.unibo.goldhunt.board.api.RevealStrategy;
 import it.unibo.goldhunt.engine.api.Position;
 
 public class FloodReveal implements RevealStrategy {
+
+    private static final int NO_ADJACENT_TRAPS = 0;
 
     /**
      * {@inheritDoc}
      */
     @Override
     public void reveal(Board b, Position p) {
-        
-    }
+        Objects.requireNonNull(b);
+        final Cell cell = b.getCell(p);
+        cell.reveal();
+        if (cell.getAdjacentTraps() == NO_ADJACENT_TRAPS) {
+            b.getAdjacentCells(p).forEach(adjacent -> {
+                Position adjacentPosition = b.getCellPosition(adjacent);
+                reveal(b, adjacentPosition);});
+            }
+        }
 
 }
