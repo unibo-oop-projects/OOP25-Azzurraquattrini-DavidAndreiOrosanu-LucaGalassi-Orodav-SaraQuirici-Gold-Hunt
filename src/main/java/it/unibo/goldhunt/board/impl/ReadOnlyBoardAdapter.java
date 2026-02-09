00ddc9df9975e328722016610bf512bab51bb520
azzurraquattrini <azzurra.quattrini@studio.unibo.it>
2026-02-09 19:16:08 +0100

    Added javadoc comments to class ReadOnlyBoardAdapter

--- src/main/java/it/unibo/goldhunt/board/impl/ReadOnlyBoardAdapter.java
+++ src/main/java/it/unibo/goldhunt/board/impl/ReadOnlyBoardAdapter.java
@@ -9,20 +9,36 @@ import it.unibo.goldhunt.board.api.ReadOnlyBoard;
 import it.unibo.goldhunt.board.api.ReadOnlyCell;
 import it.unibo.goldhunt.engine.api.Position;
 
-public class ReadOnlyBoardAdapter implements ReadOnlyBoard {
+/**
+ * This class implements {@link ReadOnlyBoard}.
+ */
+public final class ReadOnlyBoardAdapter implements ReadOnlyBoard {
 
     private final Board board;
 
+    /**
+     * {@code ReadOnlyBoardAdapter}'s constructor.
+     * It creates a read-only view of the given {@link Board}.
+     * 
+     * @param board the given board
+     * @throws NullPointerException if {@code board} is {@code null}
+     */
     public ReadOnlyBoardAdapter(final Board board) {
         Objects.requireNonNull(board);
         this.board = board;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int boardSize() {
         return this.board.getBoardSize();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public ReadOnlyCell cellAt(final Position p) {
         return new ReadOnlyCellAdapter(board.getCell(p));
