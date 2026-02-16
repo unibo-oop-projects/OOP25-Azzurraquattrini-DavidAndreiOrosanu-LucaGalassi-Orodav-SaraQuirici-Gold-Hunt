a245840137c211f344f6a2757ed03ac1e80ce67f
Danilo Pianini <danilo.pianini@unibo.it>
2026-02-16 17:33:41 +0100

    ci: add the OOP machinery

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,15 @@
 import org.gradle.api.tasks.testing.logging.TestLogEvent
-
 plugins {
     java
     application
     id("com.gradleup.shadow") version "9.3.1"
     id("org.danilopianini.gradle-java-qa") version "1.164.0"
+
+    id("org.danilopianini.unibo-oop-gradle-plugin") version "1.1.79"
 }
+
+java { toolchain { languageVersion.set(JavaLanguageVersion.of(21)) } }
+
 
 repositories {
     mavenCentral()
